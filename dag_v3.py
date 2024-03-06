707e6401a28bea4a4a525b040f405a8ae0cfca0f
Lesseux <64799181+Lesseux@users.noreply.github.com>
2024-03-06 12:34:55 -0300

    Add files via upload
    Airflow dag to set up the pipeline.

---
+++ dag_v3.py
@@ -0,0 +1,80 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocSubmitJobOperator,
+    DataprocCreateClusterOperator,
+    DataprocDeleteClusterOperator,
+    ClusterGenerator
+)
+
+GOOGLE_CONN_ID = "google_cloud_default"
+PROJECT_ID = "data-pipeline-brewery"
+BUCKET_NAME = "data-pipeline-brewery"
+CLUSTER_NAME = "cluster-data-pipeline"
+REGION = "us-central1"
+PYSPARK_URI = "gs://data-pipeline-brewery/pyspark_operators/task1_3.py"
+
+
+PYSPARK_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {"main_python_file_uri": PYSPARK_URI},
+}
+
+
+CLUSTER_CONFIG = ClusterGenerator(
+    project_id=PROJECT_ID,
+    zone="us-central1-c",
+    master_machine_type="n1-standard-2",
+    worker_machine_type="n1-standard-2",
+    num_workers=2,
+    worker_disk_size=30,
+    master_disk_size=50,
+    storage_bucket=BUCKET_NAME,
+).make()
+
+
+default_args = {
+    'owner': 'GGL',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'start_date':  days_ago(2),
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+with DAG('BreweryPipeLine', schedule_interval='@once', default_args=default_args) as dag:
+    start_pipeline = DummyOperator(
+        task_id='start_pipeline',
+        dag=dag
+        )
+    create_cluster = DataprocCreateClusterOperator(
+        task_id="create_cluster",
+        project_id=PROJECT_ID,
+        cluster_config=CLUSTER_CONFIG,
+        region=REGION,
+        cluster_name=CLUSTER_NAME,
+    )
+    pyspark_task = DataprocSubmitJobOperator(
+        task_id="task_data_lake_etl",
+        job=PYSPARK_JOB,
+        region=REGION,
+        project_id=PROJECT_ID
+    )
+    delete_cluster = DataprocDeleteClusterOperator(
+        task_id="delete_cluster",
+        project_id=PROJECT_ID,
+        cluster_name=CLUSTER_NAME,
+        region=REGION,
+    )
+    finish_pipeline = DummyOperator(
+        task_id='finish_pipeline',
+        dag=dag
+    )
+
+
+start_pipeline >> create_cluster >> pyspark_task >> delete_cluster >> finish_pipeline
